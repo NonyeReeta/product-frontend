c96ec3931ace6029d22e55d6b95e1b6ae90b523f
nonyereeta <nonyeibeanu@gmail.com>
2023-04-25 12:03:24 +0100

    added tables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'product-frontend';
+  showApprove: boolean = true;
+  showApproved: boolean = false;
+
+  toggleApproved() {
+    this.showApproved = true;
+    this.showApprove = false;
+  }
+  toggleApprove() {
+    this.showApproved = false;
+    this.showApprove = true;
+  }
 }
